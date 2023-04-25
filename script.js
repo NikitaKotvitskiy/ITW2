5f7fa45f6601271673e09b2b3afcd66a0a7095b0
NikitaKotvitskiy <107050567+NikitaKotvitskiy@users.noreply.github.com>
2023-04-25 03:43:37 +0200

    Adaptive menu added

--- script.js
+++ script.js
@@ -57,22 +57,13 @@ navButtons.forEach(tab => {
           sec.classList.add('right');
         }
       });
+      document.querySelector('#navigation').classList.remove('active');
+      document.getElementById('nav-button').classList.remove('active');
     }
   });
 });
 
 function navButton() {
-  document.querySelector('.header-navigation nav').classList.toggle('showed');
+  document.querySelector('#navigation').classList.toggle('active');
+  document.getElementById('nav-button').classList.toggle('active');
 }
-
-/*
-const lifeTimer = document.getElementById("live-timer");
-const birthDate = new Date("May 14, 2002 00:00:00");
-
-month_diff = Date.now() - birthDate.getTime();
-age_dt = new Date(month_diff);   
-year = age_dt.getUTCFullYear();  
-age = Math.abs(year - 1970);
-
-lifeTimer.innerText = age;
-*/
